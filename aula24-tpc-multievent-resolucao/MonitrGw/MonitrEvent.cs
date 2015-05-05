5cfbc1760a7034ad83171ed954f83f3bf72674d6
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2015-05-05 14:09:06 +0100

    aula 24

--- aula24-tpc-multievent-resolucao/MonitrGw/MonitrEvent.cs
+++ aula24-tpc-multievent-resolucao/MonitrGw/MonitrEvent.cs
@@ -4,38 +4,93 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace MonitrGw
 {
     public class MonitrEvent
     {
         private readonly DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private Dictionary<Type, Delegate> eventDic = new Dictionary<Type, Delegate>();
 
-        public event LastNewsEventHandler LastNewsEvent;
-        public event StockCompetitorsEventHandler StockCompetitorsEvent; 
-        public event StockAnalysisEventHandler StockAnalysisEvent;
-
-        public void OnLastNewsEvent() {
-            MonitrMarketData data = MonitrApi.GetLastNews();
-            var tmp = LastNewsEvent;
-            if (tmp != null) 
+        private void AddHandler(Delegate h)
+        {
+            Delegate aux;
+            if (eventDic.TryGetValue(h.GetType(), out aux))
             {
-                DateTime date = start.AddMilliseconds(data.Time).ToLocalTime();
-                tmp(data.Title, data.Link, date);
+                aux = Delegate.Combine(aux, h); //aux += h; // !!!1 Erro e compilação nao sabe o tipo destino do cast
+                eventDic[h.GetType()] = aux;
+            }
+            else
+                eventDic.Add(h.GetType(), h);
+        }
+        private void RemoveHandler(Delegate h)
+        {
+            Delegate aux;
+            if (eventDic.TryGetValue(h.GetType(), out aux))
+            {
+                aux = Delegate.Remove(aux, h);
+                eventDic[h.GetType()] = aux;
             }
         }
+
+        public event LastNewsEventHandler LastNewsEvent
+        {
+            add { AddHandler(value); }
+            remove { RemoveHandler(value); }
+        }
+        public event StockCompetitorsEventHandler StockCompetitorsEvent
+        {
+            add { AddHandler(value); }
+            remove { RemoveHandler(value); }
+        }
+        public event StockAnalysisEventHandler StockAnalysisEvent
+        {
+            add { AddHandler(value); }
+            remove { RemoveHandler(value); }
+        }
+
+        public void OnLastNewsEvent()
+        {
+            MonitrMarketData data = MonitrApi.GetLastNews();
+            Delegate aux;
+            if (eventDic.TryGetValue(typeof(LastNewsEventHandler), out aux))
+            {
+                LastNewsEventHandler h = ((LastNewsEventHandler)aux);
+                h(data.Title, data.Link, start.AddMilliseconds(data.Time).ToLocalTime());
+            }
+        }
+
 
         public void OnStockCompetitorsEvent(string stockSymbol)
         {
             List<string> data = MonitrApi.GetStockCompetitors(stockSymbol);
-            var tmp = StockCompetitorsEvent;
-            if (tmp != null) tmp(stockSymbol, data);
+            Delegate aux;
+            if (eventDic.TryGetValue(typeof(StockCompetitorsEventHandler), out aux))
+            {
+                StockCompetitorsEventHandler h = ((StockCompetitorsEventHandler)aux);
+                h(stockSymbol, data);
+            }
+
         }
 
         public void OnStockAnalysisEvent(string stockSymbol)
         {
             MonitrAnalysisData data = MonitrApi.GetStockAnalysis(stockSymbol);
-            var tmp = StockAnalysisEvent;
-            if (tmp != null) tmp(data);
+            Delegate aux;
+            if (eventDic.TryGetValue(typeof(StockAnalysisEventHandler), out aux))
+            {
+                StockAnalysisEventHandler h = ((StockAnalysisEventHandler)aux);
+                h(data);
+            }
+
+        }
+
+        private void OnEvent(Type eventType, params Object [] args) { 
+            Delegate aux;
+            if (eventDic.TryGetValue(eventType, out aux)) {
+                aux.DynamicInvoke(args); // Chamada via reflexão
+            }
+            
         }
     }
 }
