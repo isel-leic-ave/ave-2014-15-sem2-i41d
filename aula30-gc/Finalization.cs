62d3e13ee2821c830cc98597a7b86a8bcdb4df1d
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2015-05-19 14:19:25 +0100

    aula 30 gc e Finalization

---
+++ aula30-gc/Finalization.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+static class App{
+
+    static void PrintRunningGC(){
+        Console.WriteLine("Running GC...");
+        GC.Collect();
+    }
+
+	public static void Main(){
+        
+        // 
+        // Uma instância de FileStream mantém um handle para um recurso nativo, i.e. um ficheiro.
+        //
+        FileStream fs = new FileStream("out.txt", FileMode.Create);
+		// Wait for user to hit <Enter>
+        Console.WriteLine("Wait for user to hit <Enter>");
+		Console.ReadLine();
+		
+        PrintRunningGC();
+        Console.WriteLine("Wait for user to hit <Enter>");
+		Console.ReadLine();
+        
+        fs.Finalize(); // Não pode ser chamado explicitamente... é chamdo pelo CG
+        
+        
+        
+        
+	}
+    
+}
--- aula30-gc/RootReferences.cs
+++ aula30-gc/RootReferences.cs
@@ -4,6 +4,20 @@ namespace GCCollectIntExample
 {
     class MyGCCollectClass
     {
+        static void PrintGeneration(object obj){
+            Console.WriteLine("Object in generation " + GC.GetGeneration(obj));
+        }
+    
+        static void PrintRunningGC(){
+            Console.WriteLine("Running GC...");
+            GC.Collect();
+        }
+    
+        static void PrintRunningGC(int n){
+            Console.WriteLine("Running GC for generation " + n);
+            GC.Collect(n);
+        }
+    
         static void Main()
         {
             Version [] myGCCol = new Version[1];
@@ -13,7 +27,31 @@ namespace GCCollectIntExample
             // Determine the maximum number of generations the system 
             // garbage collector currently supports.
             Console.WriteLine("The highest generation is {0}", GC.MaxGeneration);
-           
+            PrintGeneration(myGCCol); //geração 0
+            
+            PrintRunningGC();
+            PrintGeneration(myGCCol); //geração 1
+            
+            PrintRunningGC();
+            PrintGeneration(myGCCol); //geração 2
+            
+            myGCCol = MakeSomeGarbage();
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            PrintGeneration(myGCCol); //geração 0 -- myGCCol refere um novo objecto
+            
+            PrintRunningGC();
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            PrintRunningGC();
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            PrintGeneration(myGCCol); //geração 2
+            
+            myGCCol = new Version[1];  // Now Some garbage can be collected
+            PrintRunningGC(0);
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            PrintRunningGC(1);
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            PrintRunningGC(2);
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
         }
 
         private const long maxGarbage = 1000;
@@ -24,8 +62,6 @@ namespace GCCollectIntExample
 
             for(int i = 0; i < maxGarbage; i++)
             {
-                // Create objects and release them to fill up memory 
-                // with unused objects.
                 vts[i] = new Version();
             }
             return vts;
